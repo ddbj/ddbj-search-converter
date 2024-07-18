760b9131a90bafab1bfdedcd738dc2ef7d50bcc8
dogrunjp <oec@dogrun.jp>
2024-07-18 12:55:48 +0900

    Updated dbxref object converter

--- src/dblink/get_dblink.py
+++ src/dblink/get_dblink.py
@@ -1,4 +1,5 @@
 import re
+import sqlite3
 from typing import List
 
 class RelationObject():
@@ -13,10 +14,10 @@ class RelationObject():
             "sra-run": r"(S|D|E)RR",
             "sra-sample": r"(S|D|E)RS",
             "sra-study": r"(S|D|E)RP",
-            "GEA": r"^E-GEA",
-            "MTB": r"^MTB",
-            "insdc": r"^CP",
-            "insdc_master": r"K",
+            "gea": r"^E-GEA",
+            "assemblies": r"^GCA",
+            "mtb": r"^MTB",
+            "insdc": r"([A-Z]{1}[0-9]{5}.[0-9]+|[A-Z]{2}[0-9]{6}|[A-Z]{2}[0-9]{6}.[0-9]+|[A-Z]{2}[0-9]{8}|[A-Z]{4}[0-9]{2}S?[0-9]{6,8})|[A-Z]{6}[0-9]{2}S?[0-9]{7,9}",
         }
 
     def identify_type(self, id:str) -> dict:
@@ -31,14 +32,13 @@ class RelationObject():
         # relation_mapでtypeに変換する・mapに含まれず値が数字で始まるIDであればtype: taxonomyを返す
         for db_type, pattern in self.relation_patterns.items():
             if re.match(pattern, id):
-                return db_type
+                return {"identifier": id, "type": db_type}
 
 
 def get_dbxref(ids:List[str]) -> dict:
     """
-    get_dblinkから関連するIDのリスト（List[str]）を取得し
-    ElasticsearchのdbXrefsのスキーマに合わせたオブジェクト（List[dict]）に整形しオブジェクトを返す
-
+    - get_dblinkから関連するIDのリスト（List[str]）を取得する
+    - typeを付加しElasticsearchのdbXrefsのスキーマに合わせたオブジェクト（List[dict]）に整形しオブジェクトを返す
     Args:
         id (str): BioSampleまたはBioProject
 
@@ -46,12 +46,62 @@ def get_dbxref(ids:List[str]) -> dict:
         List[dict]: [{"identifier": "", "type": ""},,]
     """
     relation = RelationObject()
-    dbxref_list = [relation.identify_type(x) for x in ids]
-    return dbxref_list
+    dbxref_list = [relation.identify_type(x) for x in ids if x != None]
+    return list(filter(None,dbxref_list))
+
+
+def get_related_ids(id:str) -> list:
+    """
+    全ての関係テーブルから指定したIDに関連するIDを取得する
+    Args:
+        id (str): 捜査対象のID
+    return: 関係テーブルの引数のIDをどちらかに含む相補的なIDリスト
+    """
+    # SQLiteデータベースのパス
+    sqlite_db_path = 'ddbj_dblink.db'
+    conn = sqlite3.connect(sqlite_db_path)
+    c = conn.cursor()
+
+    # 関係テーブルのリスト
+    table_names = [
+        "assembly_genome2bp",
+        "assembly_genome2bs",
+        "bioproject2biosample",
+        "bioproject_umbrella2bioproject",
+        "biosample2bioproject",
+        "gea2bioproject",
+        "gea2biosample",
+        "insdc2bioproject",
+        "insdc2biosample",
+        "insdc_master2bioproject",
+        "insdc_master2biosample",
+        "mtb_id_bioproject",
+        "mtb_id_biosample",
+        "ncbi_biosample_bioproject",
+        "trace_biosample_taxon2bs"
+    ]
+
+    related_ids = []
+    for table_name in table_names:
+        c.execute(f'SELECT * FROM {table_name} WHERE field1 = ? OR field2 = ?', (id, id))
+        for row in c.fetchall():
+            related_ids.append(row[0])
+            related_ids.append(row[1])
+
+    conn.close()
+    # Todo: related_idsから重複を削除する・自分自身を削除する
+    related_ids = [i for i in related_ids if i != id]
+    return list(set(related_ids))
 
 
 if __name__ == "__main__":
-    id_lst = []
-    print(get_dbxref(id_lst))
-
+    # sample ids
+    id_lst = [
+        "PRJNA3",
+        "PRJNA5",
+        "SAMN00000002"
+    ]
+    for id in id_lst:
+        related_ids = get_related_ids(id)
+        print(get_dbxref(related_ids))
 
