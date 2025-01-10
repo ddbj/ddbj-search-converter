025a707372471f25dd9b4b4827002fd5758398e1
dogrunjp <oec@dogrun.jp>
2025-01-10 16:56:35 +0900

    process for id relation db updated

--- src/dblink/create_id_relation_table.py
+++ src/dblink/create_id_relation_table.py
@@ -1,6 +1,23 @@
 # encoding:utf-8
 import sqlite3
 
+
+table_list = [
+    "study_bioproject",
+    "study_experiment",
+    "study_submission",
+    "experiment_study",
+    "experiment_bioproject",
+    "experiment_sample",
+    "experiment_biosample",
+    "sample_experiment",
+    "sample_biosample",
+    "analysis_submission",
+    "run_experiment",
+    "run_sample",
+    "run_biosample"
+]
+
 def create_table_sql(table_name):
     
     table_sql = f"""
@@ -9,7 +26,7 @@ def create_table_sql(table_name):
         id1 VARCHAR(16)
     );
     """
-    print("table: ", table_sql)
+    #print("table: ", table_sql)
     return table_sql
 
 
@@ -32,24 +49,22 @@ def initialize_table(db):
     drop_all,create_tableを呼び
     sraの関係テーブルを初期化する
     """
-    table_list = [
-        "study_bioproject",
-        "study_experiment",
-        "experiment_study",
-        "experiment_bioproject",
-        "experiment_sample",
-        "experiment_biosample",
-        "sample_experiment",
-        "sample_biosample",
-        "analysis_submission",
-        "run_experiment",
-        "run_sample",
-        "run_biosample"
-    ]
     conn = sqlite3.connect(db)
     for t in table_list:
         drop_table(conn, t)
         create_table(conn, t)
+
+
+def create_indexes(db):
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+
+    for t in table_list:
+        sql0 = f"CREATE INDEX index_{t}_0 ON {t}(id0)"
+        sql1 = f"CREATE INDEX index_{t}_1 ON {t}(id1)"
+        cur.execute(sql0)
+        cur.execute(sql1)
+        conn.commit()
 
     
 if __name__ == "__main__":
