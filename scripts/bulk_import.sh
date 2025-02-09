a0e9e5bc64f31f5aa5b942eac94e82cb65467ddd
suecharo <suecharo@sator.co.jp>
2025-02-09 18:52:56 +0900

    Remove unused scripts

--- scripts/bulk_import.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-HEAD="Content-Type:application/x-ndjson"
-URL="localhost:9200/_bulk"
-
-for i in $(ls bs_1_*.jsonl); do
-    # echo "curl -s -H $HEAD -XPOST $URL --data-binary \"@$i\""
-    curl -s -H $HEAD -XPOST $URL --data-binary "@$i" >logs/$i.logs
-done
